7bf43606ce8ab1a3de713d9f09574cf4e71716ec
Gaurav Jetley <gauravjetley@users.noreply.github.com>
2015-04-12 16:06:33 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,17 +1,17 @@
-## Getting full dataset
+## full file
 hpc <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 
-## Subsetting the data
+## Subset
 data <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 
-## Converting dates
+## Date and Time
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Plot 2
+## Plotting
 plot(data$Global_active_power~data$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
